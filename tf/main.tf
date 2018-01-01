fe1a7857644c46052d6b8486fcf17b727376e104
asim <asimawan.42@gmail.com>
2018-01-01 14:43:48 +0500

    Change s3 backend key

--- tf/main.tf
+++ tf/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "${var.s3_backend_bucket}"
-    key    = "efs/terraform.tfstate"
+    key    = "terraform_states/efs/terraform.tfstate"
     region = "${var.region}"
   }
 }
